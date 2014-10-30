d5984a9f45b4f850ce4364757e5f0359d8adf2cc
Greg Naughton <greg@naughton.org>
2014-10-30 08:48:55 -0600

    project space ready for nanotts rewrite

--- src/nanotts.cpp
+++ src/nanotts.cpp
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *   Improved pico2wave.
+ *   Nanotts == improved pico2wave.
  *   Convert text to .wav using svox text-to-speech system.
  *
  */
